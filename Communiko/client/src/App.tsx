8bbf876f7833d2696cba4fb4754bc7c8d4571dbe
Sergei Kamianetskii <i@ksergey.ru>
2023-04-26 03:00:55 +0300

    Представление активностей обновлено

--- Communiko/client/src/App.tsx
+++ Communiko/client/src/App.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import { Activeness, ActivenessItem } from './ActivenessItem';
 
 function App() {
-  const [activeness, setActiveness] = useState();
+  const [activeness, setActiveness] = useState([]);
   useEffect(() => {
     axios.get('http://localhost:11222/api/Activeness/')
       .then(res => {
-        console.log(res);
         setActiveness(res.data);
       })
   }, []);
@@ -16,7 +16,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        {
+          activeness.map((e: Activeness) => (
+            <div key={e.id}>
+              <label htmlFor="cdc"></label>
+              <ActivenessItem activenessItem={e} />
+            </div>
+          ))
+        }
       </header>
     </div>
   );
