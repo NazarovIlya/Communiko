ab71da7faebc381293cb596a922713722af23af8
Sergei Kamianetskii <i@ksergey.ru>
2023-04-28 02:17:28 +0300

    Обновление ActivenessController: BaseController

--- Communiko/PresentationAPI/Controllers/ActivenessController.cs
+++ Communiko/PresentationAPI/Controllers/ActivenessController.cs
@@ -6,31 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationAPI.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ActivenessController : ControllerBase
+  public class ActivenessController : BaseController
   {
-    private readonly IMediator mediator;
-    public ActivenessController(IMediator mediator)
-    {
-      this.mediator = mediator;
-    }
-
     [HttpGet("{id}")]
     public async Task<ActionResult<Activeness>> Get(Guid id)
     {
-      return await mediator.Send(new ItemActivities.Query() { Id = id });
+      return await Mediator.Send(new ItemActivities.Query() { Id = id });
     }
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Activeness>>> Get()
     {
-      return await mediator.Send(new ItemsActivities.Query());
+      return await Mediator.Send(new ItemsActivities.Query());
     }
     [HttpPost]
     public async Task<IActionResult> Create(Activeness activeness)
     {
       Debug.WriteLine(activeness);
-      return Ok(await mediator.Send(new CreateActivities.Command()
+      return Ok(await Mediator.Send(new CreateActivities.Command()
       {
         Item = activeness
       }));
@@ -39,7 +31,7 @@ namespace PresentationAPI.Controllers
     public async Task<IActionResult> Edit(Guid id, Activeness activeness)
     {
       activeness.Id = id;
-      return Ok(await mediator.Send(new EditActivities.Command
+      return Ok(await Mediator.Send(new EditActivities.Command
       {
         Item = activeness
       }));
@@ -47,7 +39,7 @@ namespace PresentationAPI.Controllers
     [HttpDelete("{id}")]
     public async Task<IActionResult> Remove(Guid id)
     {
-      return Ok(await mediator.Send(new RemoveActivities.Command
+      return Ok(await Mediator.Send(new RemoveActivities.Command
       {
         Id = id
       }));
