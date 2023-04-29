38d0c501e315c287107fdfef32ba17d005f1d9de
Sergei Kamianetskii <i@ksergey.ru>
2023-04-29 03:26:09 +0300

    Обновлена структура папок клиента

--- Communiko/client/src/layout/App.tsx
+++ Communiko/client/src/layout/App.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios'
-import { Activeness, ActivenessItem } from './ActivenessItem';
+import { ActivenessItem } from '../components/activeness/ActivenessItem';
+import { Activeness } from "../model/Activeness";
 import { Button, Header } from 'semantic-ui-react';
 
 function App() {
-  const [activeness, setActiveness] = useState([]);
+  const [activeness, setActiveness] = useState<Activeness[]>([]);
   useEffect(() => {
-    axios.get('http://localhost:11222/api/Activeness/')
+    axios.get<Activeness[]>('http://localhost:11222/api/Activeness/')
       .then(res => {
         setActiveness(res.data);
       })
@@ -19,15 +18,13 @@ function App() {
       <Header as='h1' >
         <img src='images/logo.png' /><label>Communiko project</label>
       </Header>
-
       {
-        activeness.map((e: Activeness) => (
+        activeness.map(e => (
           <div key={e.id}>
             <ActivenessItem activenessItem={e} />
           </div>
         ))
       }
-
     </div>
   );
 }
