6c9d3e57da88f316aba3a1289575b42824c71d90
Sergei Kamianetskii <i@ksergey.ru>
2023-05-04 10:52:20 +0300

    Перерисована ActivenessItem

--- Communiko/client/src/components/activeness/ActivenessItem.tsx
+++ Communiko/client/src/components/activeness/ActivenessItem.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activeness } from "../../model/Activeness";
 import { useRepository } from "../../repository/Repository";
 import { observer } from "mobx-react-lite";
@@ -10,40 +10,48 @@ interface PropsActivenessItem {
 
 export default observer(function ActivenessItem(
   { activenessItem }: PropsActivenessItem) {
-  const { repo } = useRepository();
-  const
-    {
-      deleteActiveness,
-      loading,
-      btnId
-    } = repo;
-
+  let activity = activenessItem;
   return (
-    <Card key={activenessItem.id} fluid>
-      <Card.Content>
-        <Card.Header>{activenessItem.title}</Card.Header>
-        <Card.Meta>{activenessItem.city}</Card.Meta>
-        <Card.Description>
-          {activenessItem.description}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui three buttons'>
-          <Button basic
-            name={`${activenessItem.id}items`}
-            loading={loading && btnId === `${activenessItem.id}items`}
-            disabled={loading}
-            onClick={(arg) => deleteActiveness(arg, activenessItem.id)}
-            color='green'>
-            Remove
-          </Button>
-          <Button basic
-            as={Link} to={`/activenessItems/${activenessItem.id}`}
-            color='green'>
-            Details
-          </Button>
-        </div>
-      </Card.Content>
-    </Card >
+    <Segment.Group color={{ color: 'red' }}>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Item.Header
+                as={Link}
+                to={`/activenessItems/${activenessItem.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>Author</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Label>
+          <Icon name='clock' />
+          {activity.pointTime}
+        </Label>
+        <Label>
+          <Icon name='marker' />
+          {activity.location}
+        </Label>
+      </Segment>
+      <Segment secondary>
+        List
+      </Segment>
+      <Segment clearing>
+        <Label style={{ marginBottom: '10px' }}>
+          <Icon name='info' />
+          {activity.description}
+        </Label>
+        <Button basic
+          as={Link}
+          to={`/activenessItems/${activenessItem.id}`}
+          color='green'
+          floated='right'
+          content='Details' />
+      </Segment>
+    </Segment.Group>
   );
 })
