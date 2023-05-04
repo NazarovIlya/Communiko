20d6c7c7cc3c11a6174630fd28a5a3ac50ce9acb
Sergei Kamianetskii <i@ksergey.ru>
2023-05-04 07:55:20 +0300

    Добавлен маршрут получения записи по id

--- Communiko/client/src/router/Router.tsx
+++ Communiko/client/src/router/Router.tsx
@@ -3,6 +3,7 @@ import App from "../layout/App";
 import HomePage from "../components/home/HomePage";
 import ActivenessItems from "../components/activeness/ActivenessItems";
 import ActivenessEditForm from "../components/activeness/ActivenessEditForm";
+import ActivenessDetails from "../components/details/ActivenessDetails";
 
 export const routes: RouteObject[] = [
   {
@@ -11,6 +12,7 @@ export const routes: RouteObject[] = [
     children: [
       { path: '', element: <HomePage /> },
       { path: 'activenessItems', element: <ActivenessItems /> },
+      { path: 'activenessItems/:id', element: <ActivenessDetails /> },
       { path: 'createActiveness', element: <ActivenessEditForm /> }
     ]
   }
