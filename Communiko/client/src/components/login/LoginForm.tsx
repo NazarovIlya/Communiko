c651093fc7fd226286f7290830601ec48b890c68
Sergei Kamianetskii <i@ksergey.ru>
2023-05-09 13:51:21 +0300

    Реагирование на ошибку входа

--- Communiko/client/src/components/login/LoginForm.tsx
+++ Communiko/client/src/components/login/LoginForm.tsx
@@ -2,13 +2,14 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import { Button, Header, Label } from "semantic-ui-react";
 import { useRepository } from "../../repository/Repository";
+import { error } from "console";
 
 export default observer(function LoginForm() {
   const { userRepo } = useRepository();
   return (
     <Formik
       initialValues={{ email: '', password: '', error: null }}
-      onSubmit={(values) => userRepo.auth(values)}
+      onSubmit={(values, { setErrors }) => userRepo.auth(values).catch(e => setErrors({ error: "Данные неверны " }))}
     >
       {({ handleSubmit, isSubmitting, errors }) => (
         <Form className='ui form' onSubmit={handleSubmit} >
