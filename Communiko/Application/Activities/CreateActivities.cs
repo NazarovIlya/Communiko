0133f012a259509402721ea24d6102d393ea25c7
Sergei Kamianetskii <i@ksergey.ru>
2023-05-11 11:01:31 +0300

    Привязка автора к активности

--- Communiko/Application/Activities/CreateActivities.cs
+++ Communiko/Application/Activities/CreateActivities.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics;
 using Application.AppConfig;
+using Application.Interface;
 using BusinessDomain.Model;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -25,15 +27,29 @@ namespace Application.Activities
     public class Handler : IRequestHandler<Command, ValidationResult<Unit>>
     {
       private readonly DataContext context;
+      public readonly IAppUserAccessor appUserAccessor;
 
-      public Handler(DataContext context)
+      public Handler(DataContext context, IAppUserAccessor appUserAccessor)
       {
+        this.appUserAccessor = appUserAccessor;
         this.context = context;
       }
 
       public async Task<ValidationResult<Unit>> Handle(Command request,
         CancellationToken cancellationToken)
       {
+
+        var user = await context.Users.FirstOrDefaultAsync(appUser =>
+                    appUser.UserName == appUserAccessor.GetUserName());
+
+        var newParticipant = new AppUserActiveness
+        {
+          AppUser = user,
+          Activity = request.Item,
+          IsAuthor = true
+        };
+        request.Item.Participants.Add(newParticipant);
+
         var missing = await context.Activities.FindAsync(request.Item.Id) == null;
         if (!missing) return ValidationResult<Unit>.Failure("Такая запись есть");
 
