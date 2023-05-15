325e993a4ccdf6d66bf15e19332c2fc0499c85be
Sergei Kamianetskii <i@ksergey.ru>
2023-05-15 23:24:10 +0300

    CommentDto + настройка AutoMapper

---
+++ Communiko/Application/ModelDto/CommentDto.cs
@@ -0,0 +1,14 @@
+using BusinessDomain.Model;
+
+namespace Application.ModelDto
+{
+  public class CommentDto
+  {
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string UserName { get; set; }
+    public string NickName { get; set; }
+    public string FullName { get; set; }
+    public DateTime Create { get; set; }
+  }
+}
